b45b07bce70fa31964c2fba95fcd0ffc241b346e
NOGISAKA Sadata <ngsksdt@gmail.com>
2019-09-16 23:16:16 +0900

    Install tor-browser on guispawn

--- guispawn/mkosi.extra/qutebrowser.config.py
+++
@@ -1,9 +0,0 @@
-c.content.images = True
-c.content.javascript.enabled = True
-c.content.plugins = True
-c.content.private_browsing = True
-c.content.proxy = "system"
-c.content.ssl_strict = True
-c.content.xss_auditing = True
-c.input.insert_mode.plugins = True
-c.window.title_format = "{private}{perc}{title}{title_sep}qutebrowser (in nspawn container)"
